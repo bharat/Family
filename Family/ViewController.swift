4ce80b40c4c13a64b07276310e08bb68db01203c
Bharat Mediratta <bharat@altschool.com>
2016-09-11 12:24:27 -0700

    Initial Commit

---
+++ Family/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Family
+//
+//  Created by Bharat Mediratta on 9/11/16.
+//  Copyright © 2016 Bharat Mediratta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
